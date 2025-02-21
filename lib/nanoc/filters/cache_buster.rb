9ccb40ed1fff0cd890fe13b1a4329b9318af98aa
ximus <maximeliron@gmail.com>
2025-02-21 12:31:00 -0800

    hackgrade to nanoc4

--- lib/nanoc/filters/cache_buster.rb
+++ lib/nanoc/filters/cache_buster.rb
@@ -5,7 +5,7 @@ module Nanoc
 
       def run(content, options = {})
         kind = options[:strategy] || (stylesheet? ? :css : :html)
-        strategy = Nanoc::Cachebuster::Strategy.for(kind , site, item)
+        strategy = Nanoc::Cachebuster::Strategy.for(kind , OpenStruct.new({ items: }), item)
         content.gsub(strategy.class::REGEX) do |m|
           begin
             strategy.apply m, $1, $2, $3, $4
@@ -30,4 +30,3 @@ module Nanoc
     end
   end
 end
-
